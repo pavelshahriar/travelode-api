9cbade5343245c54cf0dbaf9f915269d5aa2320c
Shahriar Pavel <shahriar.pavel@sharecare.com>
2018-03-25 02:44:48 -0400

    Host name fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const config = require('./config');
+const host = process.env.HOST;
 const port = process.env.PORT || config.port; // Important for Heroku
 
 const express = require('express');
@@ -13,7 +14,7 @@ module.exports = server; // for testing
 
 // Init Swagger UI
 const swaggerDoc = yaml.load(config.appRoot + '/api/swagger/swagger.yaml');
-swaggerDoc.host = config.host + ':' + port;
+swaggerDoc.host = host + ':' + port;
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   // Serve the Swagger documents and Swagger UI
   server.use(middleware.swaggerUi());
@@ -32,5 +33,5 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
 });
 
 server.get('/', function (req,res) {
-  res.send('Welcome to Travelode API v0.1 ! <br>Please load : ' + config.host + ':' + port +'/docs')
+  res.send('Welcome to Travelode API v0.1 ! <br>Please load : ' + host + ':' + port +'/docs')
 });

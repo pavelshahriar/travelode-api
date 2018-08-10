1a373ab9cf3cff31e0982f1f6e54f7d0f9ea4d95
Pavel Shahriar <shahriar.tanvir@gmail.com>
2018-08-09 21:40:51 -0400

    New SQL scheme

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `travelodeApi` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `travelodeApi`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: travelodeApi
@@ -27,7 +29,7 @@ CREATE TABLE `category` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +77,7 @@ CREATE TABLE `media` (
   KEY `mediaLocationId_idx` (`locationId`),
   CONSTRAINT `mediaLocationId` FOREIGN KEY (`locationId`) REFERENCES `location` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `mediaUserId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +116,7 @@ CREATE TABLE `travelode` (
   KEY `travelodePrivacyId_idx` (`privacy`),
   CONSTRAINT `travelodePrivacyId` FOREIGN KEY (`privacy`) REFERENCES `privacy` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `travelodeUserId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,25 +130,29 @@ CREATE TABLE `travelode_media` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `travelodeId` int(11) NOT NULL,
   `mediaId` int(11) NOT NULL,
-  `rollNo` int(11) NOT NULL,
+  `rollNo` int(11) NOT NULL DEFAULT '0',
   `privacy` int(11) NOT NULL,
   `title` varchar(45) NOT NULL,
   `caption` varchar(200) DEFAULT NULL,
   `displayDate` datetime NOT NULL,
   `displayLocationId` int(11) DEFAULT NULL,
-  `attachmentDate` datetime NOT NULL,
   `isCover` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `isCover` (`isCover`),
+  UNIQUE KEY `travelodeMediaTravelodeIdMediaId_Unique_idx` (`travelodeId`,`mediaId`),
   KEY `travelodeMediaMediaId_idx` (`mediaId`),
   KEY `travelodeMediaDisplayLocationId_idx` (`displayLocationId`),
   KEY `travelodeMediaTravelodeId_idx` (`travelodeId`),
   KEY `travelodeMediaPrivacyId_idx` (`privacy`),
+  KEY `travelodeMediaisCover` (`isCover`),
+  KEY `travelodeMediaCreated` (`created`),
+  KEY `travelodeMediaUpdated` (`updated`),
   CONSTRAINT `travelodeMediaDisplayLocationId` FOREIGN KEY (`displayLocationId`) REFERENCES `location` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   CONSTRAINT `travelodeMediaMediaId` FOREIGN KEY (`mediaId`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `travelodeMediaPrivacyId` FOREIGN KEY (`privacy`) REFERENCES `privacy` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `travelodeMediaTravelodeId` FOREIGN KEY (`travelodeId`) REFERENCES `travelode` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,13 +168,14 @@ CREATE TABLE `travelode_media_category` (
   `mediaId` int(11) NOT NULL,
   `categoryId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `travelodeMediaCategoryTravelodeIdMediaIdCategoryId_Unique_idx` (`travelodeId`,`mediaId`,`categoryId`),
   KEY `travelodeMediaCategoryTravelodeId_idx` (`travelodeId`),
   KEY `travelodeMediaCategoryMediaId_idx` (`mediaId`),
   KEY `travelodeMediaCategoryCategoryId_idx` (`categoryId`),
   CONSTRAINT `travelodeMediaCategoryCategoryId` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `travelodeMediaCategoryMediaId` FOREIGN KEY (`mediaId`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `travelodeMediaCategoryTravelodeId` FOREIGN KEY (`travelodeId`) REFERENCES `travelode` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,13 +190,15 @@ CREATE TABLE `user` (
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `fullname` varchar(145) DEFAULT NULL,
-  `photo` varchar(150) DEFAULT NULL,
+  `photo` int(11) DEFAULT NULL,
   `created` datetime NOT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `UserPhotoId_idx` (`photo`),
+  CONSTRAINT `UserPhotoId` FOREIGN KEY (`photo`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -201,4 +210,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-25  1:02:24
+-- Dump completed on 2018-08-09 21:35:54

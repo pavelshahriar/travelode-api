02ae88f19beec26adab55556f9f2e18fac890750
Tamal Sen <tamal.2k14@gmail.com>
2018-08-31 14:20:37 +0600

    added TravelodeMediaCategoryId validation

--- api/services/travelodeMediaCategoryService.js
+++ api/services/travelodeMediaCategoryService.js
@@ -22,6 +22,21 @@ class travelodeMediaCategoryService {
       callback(null, found);
     });
   }
+
+  isTravelodeMediaCategoryValid(id, callback) {
+    const query = db.query('SELECT '+ this.selectItems +' FROM ' + this.tableName + ' WHERE id = ?',
+    [id],
+    function(err, result) {
+      console.log(query.sql);
+      if (err) {
+        console.error(err);
+        callback(err, null);
+      }
+
+      let found = (result.length !== 0) ? true : false;
+      callback(null, found);
+    });
+  }
 }
 
 module.exports = new travelodeMediaCategoryService('travelode_media_category', 'id, travelodeId, mediaId, categoryId');
